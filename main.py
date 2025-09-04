87b5d43fe2dd62678e2682a3e11625c70db69a39
Superjake03 <jacoblopez0308@gmail.com>
2025-09-03 19:55:41 -0500

    Circular shape class

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-        delta_time = clock.tick(60)
-        dt = delta_time / 1000
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
