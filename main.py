eff72bb0226b6c08ac22e39124a31147d837ff52
Superjake03 <jacoblopez0308@gmail.com>
2025-09-04 17:09:53 -0500

    Using pygame Object()

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
@@ -13,11 +16,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
-        player.update(dt)
         dt = clock.tick(60) / 1000
 
 
